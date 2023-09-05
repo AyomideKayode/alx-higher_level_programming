777ae997a4beef40baf1c0c1356b134cdfe51f19
Ayomide <ayomidekay7@gmail.com>
2023-09-05 14:59:12 +0100

    Refactor code for Task1 - Output differences.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,9 @@ if number > 0:
 else:
     ld = number % (-10)
 
-if number > 5:
+if ld > 5:
     print(f"Last digit of {number:d} is {ld:d} and is greater than 5")
-elif number == 0:
+elif ld == 0:
     print(f"Last digit of {number:d} is {ld:d} and is 0")
 else:
     print(f"Last digit of {number:d} is {ld:d} and is less than 6 and not 0")
