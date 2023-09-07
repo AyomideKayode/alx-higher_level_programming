81b57382ea76c75dce0263fc32f29f326dbc9fd8
Ayomide <ayomidekay7@gmail.com>
2023-09-07 13:32:14 +0100

    Task 1 modified. Checker, please!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    # Import the functions from calculator_1.py
     from calculator_1 import add, sub, mul, div
 
-    # Assign the values to variables a and b
     a = 10
     b = 5
 
-    # Print the results
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
