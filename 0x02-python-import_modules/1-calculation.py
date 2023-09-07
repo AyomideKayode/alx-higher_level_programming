5b384064718342b09b28490bbe41c2a536c30c5b
Ayomide <ayomidekay7@gmail.com>
2023-09-07 13:13:36 +0100

    Task 1: Function to do Maths.

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+	# Import the functions from calculator_1.py
+	from calculator_1 import add, sub, mul, div
+
+	# Assign the values to variables a and b
+	a = 10
+	b = 5
+
+	# Call each imported function with a and b as arguments
+	result_add = add(a, b)
+	result_sub = sub(a, b)
+	result_multiply = mul(a, b)
+	result_divide = div(a, b)
+
+	# Print the results
+	print(f"{a} + {b} = {result_add}")
+	print(f"{a} - {b} = {result_sub}")
+	print(f"{a} * {b} = {result_multiply}")
+	print(f"{a} / {b} = {result_divide}")
