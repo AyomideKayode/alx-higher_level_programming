2e34d0626678f8fc8b603efa459887795cc852f1
Ayomide <ayomidekay7@gmail.com>
2023-09-07 13:24:34 +0100

    Task 1: Removed f-string style of formatting and fixed indentations.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-	# Import the functions from calculator_1.py
-	from calculator_1 import add, sub, mul, div
+    # Import the functions from calculator_1.py
+    from calculator_1 import add, sub, mul, div
 
-	# Assign the values to variables a and b
-	a = 10
-	b = 5
+    # Assign the values to variables a and b
+    a = 10
+    b = 5
 
-	# Call each imported function with a and b as arguments
-	result_add = add(a, b)
-	result_sub = sub(a, b)
-	result_multiply = mul(a, b)
-	result_divide = div(a, b)
-
-	# Print the results
-	print("{:d} + {:d} = {:d}".format(a, b, result_add))
-	print("{:d} - {:d} = {:d}".format(a, b, result_sub))
-	print("{:d} * {:d} = {:d}".format(a, b, result_multiply))
-	print("{:d} / {:d} = {:d}".format(a, b, result_divide))
+    # Print the results
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
