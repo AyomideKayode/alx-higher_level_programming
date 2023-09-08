ff83ecd4c3178c3082bb445fa872c7d73b1a2ef5
Ayomide <ayomidekay7@gmail.com>
2023-09-08 19:10:26 +0100

    trying things

--- 0x03-python-data_structures/main/listcomprehension.py
+++ 0x03-python-data_structures/main/listcomprehension.py
@@ -11,3 +11,8 @@ for c in range(3, 15):
 	cubes.append(c**3)
 
 print(cubes)
+
+
+#  instaed of the loop, another way might be to:
+new_squares = [x**2 for x in range(15)]
+print(f"New square style is: {new_squares}")
