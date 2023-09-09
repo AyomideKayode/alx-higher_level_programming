ec99b542eee100cc0939b87162f8ade216f63643
Ayomide <ayomidekay7@gmail.com>
2023-09-09 19:04:05 +0100

    Task 9: Find the MAX.🎉

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,8 @@ def max_integer(my_list=[]):
     if not my_list:  # check if list is empty
         return None
     max_int = my_list[0]  # initially set 1st idx as max int
-    for i in my_list[1:]:  # starting from 2nd idx since 1st has been initialised
+    # starting from 2nd idx since 1st has been initialised
+    for i in my_list[1:]:
         if i > max_int:
             max_int = i
     return max_int
