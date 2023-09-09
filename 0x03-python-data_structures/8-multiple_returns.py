abc41bfd14afbbe46968f4aaf1203a5f67a684ba
Ayomide <ayomidekay7@gmail.com>
2023-09-09 18:38:24 +0100

    Task 8: More returns. Hot fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    blank = (0, None)  # tuple to return if sentence is empty
     s_len = len(sentence)   # length of sentence
-    present = (s_len, sentence[0])    # new tuple
     if s_len == 0:
-        return blank
+        return (0, None)
     else:
+        present = (s_len, sentence[0])    # new tuple
         return present
