47832d885796d8f72c71adadb1d05fbf22b37ea3
Kazzywiz <ayomidekay7@gmail.com>
2023-09-13 16:36:27 +0100

    Task 3: Common elements in 2 sets.👍

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,7 +3,7 @@
 def common_elements(set_1, set_2):
     commonWords = set()
     for words in set_1:
-        if words in  set_2:
+        if words in set_2:
             commonWords.add(words)
     return commonWords
 
