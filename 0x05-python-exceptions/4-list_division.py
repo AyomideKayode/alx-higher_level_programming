51e13f9e33968e0772fb88f08e96d52dd3e25efb
Ayomide <ayomidekay7@gmail.com>
2023-09-25 15:50:32 +0100

    Task 4: Divide a list. Code refactor. Checker abeg.😫

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,17 +8,17 @@ def list_division(my_list_1, my_list_2, list_length):
 
         try:
             div = my_list_1[x] / my_list_2[x]
-
         except (ValueError, ZeroDivisionError):
             print("division by 0")
-
+            div = 0  # set division result to 0
         except (TypeError):
             print("wrong type")
-
+            div = 0
         except (IndexError):
             print("out of range")
+            div = 0
 
         finally:
-            list_div.append(div)
+            list_div.append(div)  # add division result into new list
 
-        return list_div
+    return list_div
