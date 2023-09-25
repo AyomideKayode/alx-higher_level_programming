cd7466b0a576169d94e3655307a97b12c31bce8d
Ayomide <ayomidekay7@gmail.com>
2023-09-25 12:36:37 +0100

    Task 0: Safe list printing. Newline fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,8 @@ def safe_print_list(my_list=[], x=0):
         for y in range(x):
             print("{}".format(my_list[y]), end="")
             idx += 1
-        print()
     except IndexError:
         pass
+    finally:
+        print()
     return idx
