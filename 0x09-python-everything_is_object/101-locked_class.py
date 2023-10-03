303717b08e3308530a3c8706a3a5e1706c3ae147
Ayomide <ayomidekay7@gmail.com>
2023-10-03 13:38:29 +0100

    Task 30: Low cost memory.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-
 class LockedClass:
     """
     Class that prevents the user from dynamically creating any new
     instance attributes, except if new attribute is called `first_name`
     """
-    __slots__ = ['first_name']
+    __slots__ = ('first_name')
+    pass
