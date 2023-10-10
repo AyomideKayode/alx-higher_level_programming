791712c2ee6fc82f43fa491056eee46d02ac3042
Kazzywiz <ayomidekay7@gmail.com>
2023-10-10 17:53:36 +0100

    Task 3: To JSON string.

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -11,9 +11,9 @@ def to_json_string(my_obj):
     """
     define funcrion to convert an object
     to its JSON representation as a string.
-    
+
     Args:
-        my_obj: The object to be serialized to JSON. 
+        my_obj: The object to be serialized to JSON.
     Returns:
         str: The JSON representation of the object as a string.
     """
