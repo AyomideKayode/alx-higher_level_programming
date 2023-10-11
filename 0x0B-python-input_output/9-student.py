701d076128b9e037d85c9f29351b1f4b555cb7ba
Kazzywiz <ayomidekay7@gmail.com>
2023-10-11 11:36:22 +0100

    Task 9: Create Student class. Pycode-fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,8 +6,10 @@ Public instance attributes:
 first_name
 last_name
 age
-Instantiation with first_name, last_name and age: def __init__(self, first_name, last_name, age):
-Public method def to_json(self): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py)
+Instantiation with first_name, last_name and age:
+def __init__(self, first_name, last_name, age):
+Public method def to_json(self): that retrieves a dictionary representation
+of a Student instance (same as 8-class_to_json.py)
 You are not allowed to import any module
 """
 
