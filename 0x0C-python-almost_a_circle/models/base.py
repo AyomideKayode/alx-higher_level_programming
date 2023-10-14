12d3e5c54486fb334a23647a3c8316469032d076
Kazzywiz <ayomidekay7@gmail.com>
2023-10-14 16:35:30 +0100

    Update Base class with staticmethod that returns JSON string representation.✨

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,11 @@ with this argument value - you can assume id is an integer
 and you don’t need to test the type of it otherwise,
 increment __nb_objects and assign the new value to
 the public instance attribute id
+Add static method def to_json_string(list_dictionaries):
+that returns the JSON string representation of list_dictionaries.
 """
+
+import json
 
 
 class Base:
@@ -27,3 +31,11 @@ class Base:
             # increment __nb_objects & assign the new value to the id attribute
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Method to return JSON string representation - list of dictionaries.
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
