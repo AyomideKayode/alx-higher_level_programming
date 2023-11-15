ae460d5ee03f5ac7e8892ca6d4b03b43805df7ce
Kazzywiz <ayomidekay7.com>
2023-11-15 20:16:36 +0100

    Hot fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -9,11 +9,11 @@
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
 
-    SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating sum'
+    SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
       FROM tv_genres
 INNER JOIN tv_show_genres
         ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_show_ratings
         ON tv_show_ratings.show_id = tv_show_genres.show_id
   GROUP BY name
-  ORDER BY rating sum DESC;
+  ORDER BY rating DESC;
