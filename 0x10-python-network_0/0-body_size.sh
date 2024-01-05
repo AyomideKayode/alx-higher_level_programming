5c9ce3dfd6e51d997464e5369be1a39fc108b5c2
Ayomide Kayode <ayomidekay7@gmail.com>
2024-01-05 13:18:00 +0100

    Script correction for body size.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that take in a URL, sends a request to it, and displays the size of the body of the response, size should be displayed in bytes
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d ''
+curl -s "$1" | wc -c
